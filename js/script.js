58c0570fd097dac91055d79e5923dea31b31afe2
Łuki Kasztaniewicz <grabowski.lukasz.88@gmail.com>
2018-04-17 00:19:50 +0200

    Add style.css & change script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
+$.ajaxSetup({ cache: false });
+
 function getQuote() {
     $.getJSON(quoteUrl, createTweet);
 }
